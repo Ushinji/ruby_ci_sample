567ea984dfa4d0ad40a37c24dba65c9dd723dbcb
Ushinji <siguma971@gmail.com>
2018-11-14 20:45:30 +0900

    テストコードの修正

--- calculation_spec.rb
+++ calculation_spec.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require './calculation'
 
 class CalculationTest < Minitest::Test
-  def test_２つの引数の足し合わせた結果を返すこと
+  def test_sum_２つの引数の足し合わせた結果を返すこと
     assert_equal(Calculation.sum(1, 2), 3)
   end
 end
